13035ddf5ef67d69b97d3fd37549e39c82f6fade
DaRubyMiner360 <darubyminer360@gmail.com>
2022-10-28 09:20:56 -0500

    Added more primitive types.

--- paracode/src/interpreter/basic_value.rs
+++ paracode/src/interpreter/basic_value.rs
@@ -4,16 +4,25 @@ use std::fmt;
 pub enum BasicValue {
     Int(i32),
     Float(f32),
+    Bool(bool),
+    String(String),
     Null,
     // TODO: Add BasicObject and BasicType
     // TODO: Add an extension point for user-defined types
+}
+impl BasicValue {
+    pub fn is_null(&self) -> bool {
+        return self == &BasicValue::Null;
+    }
 }
 
 impl fmt::Display for BasicValue {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::Int(n) => write!(f, "{}", n),
-            Self::Float(n) => write!(f, "{}", n),
+            Self::Int(val) => write!(f, "{}", val),
+            Self::Float(val) => write!(f, "{}", val),
+            Self::Bool(val) => write!(f, "{}", val),
+            Self::String(val) => write!(f, "{}", val),
             Self::Null => write!(f, "Null"),
         }
     }
