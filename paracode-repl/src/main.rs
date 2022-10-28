c67687fcc67356bbf42a53ac71b5c063991f8223
DaRubyMiner360 <darubyminer360@gmail.com>
2022-10-28 07:51:03 -0500

    Reset the repl to a simple hello world application to get it to compile until the language is ready.

--- paracode-repl/src/main.rs
+++ paracode-repl/src/main.rs
@@ -1,21 +1,3 @@
-use paracode::parser::parse;
-use std::io::{self, Write};
-
-fn main() -> io::Result<()> {
-    let stdin = io::stdin();
-    let mut stdout = io::stdout();
-
-    let mut input = String::new();
-
-    loop {
-        write!(stdout, "→ ")?;
-        stdout.flush()?;
-
-        stdin.read_line(&mut input)?;
-
-        let parse = parse(&input);
-        println!("{}", parse.debug_tree());
-
-        input.clear();
-    }
+fn main() {
+    println!("Hello, world!");
 }
